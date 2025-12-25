2ccecabf5cb483d4244e215c4199533223ff0130
Samrat Saha <samratsaha9635@gmail.com>
2025-12-25 19:07:14 +0530

    Export SERVER_URL from env configuration

--- config/env.js
+++ config/env.js
@@ -1,16 +1,20 @@
 import { config } from "dotenv";
 import process from "node:process";
 
-config({path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({
+  path: `.env.${process.env.NODE_ENV || "development"}.local`,
+});
 
 export const {
-    PORT, 
-    NODE_ENV, 
-    DB_URI, 
-    JWT_SECRET, 
-    JWT_EXPIRES_IN,
-    ARCJET_ENV, ARCJET_KEY,
-    QSTASH_TOKEN, QSTASH_URL,
-    EMAIL_PASSWORD,
+  PORT,
+  NODE_ENV,
+  SERVER_URL,        
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+  QSTASH_TOKEN,
+  QSTASH_URL,
+  EMAIL_PASSWORD,
 } = process.env;
-
